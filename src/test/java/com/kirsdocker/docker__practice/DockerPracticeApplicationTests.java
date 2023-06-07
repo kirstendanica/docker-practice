2d00462645180f3d1f9818b5b8050e5dba532088
kirsten d <kirdanic@gmail.com>
2023-06-06 22:04:04 -0400

    Initial commit

---
+++ src/test/java/com/kirsdocker/docker__practice/DockerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirsdocker.docker__practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
